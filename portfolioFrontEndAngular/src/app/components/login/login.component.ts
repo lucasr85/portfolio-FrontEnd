d3ba2aa984ab28baa188b49bc75d6e9f5d50ecda
Lucas Robles <robles.lucasm@gmail.com>
2022-10-18 16:01:35 -0300

    login

--- portfolioFrontEndAngular/src/app/components/login/login.component.ts
+++ portfolioFrontEndAngular/src/app/components/login/login.component.ts
@@ -39,14 +39,14 @@ onLogin(): void{
       this.tokenService.setUserName(data.nombreUsuario);
       this.tokenService.setAuthorities(data.authorities);
       this.roles=data.authorities;
-      this.router.navigate(['']);
+      this.router.navigate([''])
     },err =>{
       this.isLogged=false;
       this.isLogginFail=true;
       this.errMsj=err.error.mensaje;
       console.log(this.errMsj);
       
-    }))
+    })
 }
 
 }
