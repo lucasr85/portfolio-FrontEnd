ac9ffccb1af1691294438a1284a56a532c63f9e2
Lucas Robles <robles.lucasm@gmail.com>
2022-10-19 01:13:06 -0300

    acerca de

--- portfolioFrontEndAngular/src/app/model/persona.model.ts
+++ portfolioFrontEndAngular/src/app/model/persona.model.ts
@@ -3,12 +3,12 @@ export class persona{
     nombre:string;
     apellido:string;
     img:string;
+    acercade:string;
 
-    constructor(nombre: string, apellido: string, img: string){
+    constructor(nombre: string, apellido: string, img: string,acercade: string){
         this.nombre=nombre;
         this.apellido=apellido;
         this.img=img;
-    }
-
-    
+        this.acercade=acercade;
+    }    
 }
