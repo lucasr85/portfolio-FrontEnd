a549ee9ecfcbfbf8a638ddeb0b6bd5093ea58c58
Lucas Robles <robles.lucasm@gmail.com>
2022-10-20 16:01:45 -0300

    exper

--- portfolioFrontEndAngular/src/app/app.module.ts
+++ portfolioFrontEndAngular/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
